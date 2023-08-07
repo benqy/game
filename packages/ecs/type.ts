6aaf29f3aefd09bd2ed31ba324b688c5702d0466
benqy <hmjlr123@gmail.com>
2023-08-07 22:43:23 +0800

    ts config

--- packages/ecs/type.ts
+++ packages/ecs/type.ts
@@ -2,9 +2,3 @@ export type EntityId = number
 
 export type TypeProto<T> = (new (...args: any[]) => T) & Function
 export type ObjectProto = TypeProto<object>
-
-
-interface GameLoop {
-  start(): void
-  stop(): void
-}

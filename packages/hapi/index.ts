286bf744f9ec496f1151228cda9ba0f5837edc9a
benqy <hmjlr123@gmail.com>
2023-08-08 22:21:27 +0800

    1

---
+++ packages/hapi/index.ts
@@ -0,0 +1,5 @@
+export class Hapi{
+  constructor(){
+    console.log('Hapi')
+  }
+}
--- packages/main/src/main.ts
+++ packages/main/src/main.ts
@@ -1,8 +1,12 @@
 import { createApp } from 'vue'
 import './style.css'
+import {Hapi} from '@benqy/hapi'
 import App from './App.vue'
 createApp(App).mount('#app')
-import { BasicGame } from "@benqy/basic-sample"
+// import { BasicGame } from "@benqy/basic-sample"
 
 // const game = new BasicGame('game')
 // game.start()
+
+new Hapi()
+
